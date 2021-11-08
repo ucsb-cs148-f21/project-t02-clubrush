c274c222787f9de55ffec95e9632988367fdd7aa
Tommy Nguyen <tommynguyen@169-231-103-114.wireless.ucsb.edu>
2021-11-08 14:42:51 -0800

    added login and signup structure

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,8 @@
 // import styled from "styled-components";
 import Container from "react-bootstrap/Container";
 import React from "react";
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Layout from "../components/Layout";
 
 export default function Signup() {
@@ -12,6 +13,35 @@ export default function Signup() {
         <div>
             <h1>Please Signup</h1>
         </div>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter First Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Last Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Form>
       </Container>
     </Layout>
   );
