06bb1b12e73985fe8569b43d9359b45bb8676fcb
Morgan6688 <shu-hsien@ucsb.edu>
2021-11-28 23:19:20 -0800

    comment connected to database

--- api/models/club.js
+++ api/models/club.js
@@ -24,7 +24,57 @@ const clubSchema = new mongoose.Schema({
     categories: {
         type: String,
         required: false
-    }
+    },
+    comments: [{
+        id: {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: false
+        },
+        userId: {
+            type: String,
+            required: true
+        },
+        parentId: {
+            type: String,
+            required: false
+        },
+        createdAt: {
+            type: String,
+            required: false
+        }
+    }]
+    // id: {
+    //     type: String,
+    //     required: true
+    // },
+    // body: {
+    //     type: String,
+    //     required: true
+    // },
+    // username: {
+    //     type: String,
+    //     required: false
+    // },
+    // userId: {
+    //     type: String,
+    //     required: true
+    // },
+    // parentId: {
+    //     type: String,
+    //     required: false
+    // },
+    // createdAt: {
+    //     type: String,
+    //     required: false
+    // }
 })
 
 const Club = mongoose.model('Club', clubSchema);
