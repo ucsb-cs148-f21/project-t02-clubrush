aa299d4f67c25339e03867066593ee75ff54d951
Tommy Nguyen <tommynguyen@169-231-103-168.wireless.ucsb.edu>
2021-12-01 14:51:42 -0800

    change sendfile path

--- api/routes/app.js
+++ api/routes/app.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 //   res.sendfile('index');
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'))
 });
 
 module.exports = router;
