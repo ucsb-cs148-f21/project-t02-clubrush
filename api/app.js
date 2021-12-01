e0e179f2f641e9d46cec97ae0e1a7d0442b26a53
Tommy Nguyen <tommynguyen@169-231-103-168.wireless.ucsb.edu>
2021-12-01 14:12:52 -0800

    adding engine specifications

--- api/app.js
+++ api/app.js
@@ -35,10 +35,17 @@ app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
 app.use('/clubs', clubsRouter);
 app.use('/comments', commentsRouter)
+
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
 // });
+
+
+app.get('*', (req, res) => {
+  console.log("test")
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+});
 
 // // error handler
 // app.use(function(err, req, res, next) {
@@ -50,11 +57,6 @@ app.use('/comments', commentsRouter)
 //   res.status(err.status || 500);
 //   res.render('error');
 // });
-
-app.get('*', (req, res) => {
-  console.log("test")
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
